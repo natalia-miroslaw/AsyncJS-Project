1fa6043636d99d8a7fd1b5effa613338c7febd6f
natalia <n.miroslaw93@gmail.com>
2022-01-09 16:52:39 +0100

    refactor (app.ts): add types

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
+"use strict";
 const table = document.getElementById('table');
-
 const getUsers = async () => {
-  const responseUsers = await fetch('http://localhost:3000/users');
-  return await responseUsers.json();
+    const responseUsers = await fetch('http://localhost:3000/users');
+    return await responseUsers.json();
 };
-
 const getCompanies = async () => {
-  const responseCompanies = await fetch('http://localhost:3000/companies');
-  return await responseCompanies.json();
+    const responseCompanies = await fetch('http://localhost:3000/companies');
+    return await responseCompanies.json();
 };
-
 const showData = async () => {
-  const companies = await getCompanies();   //await, bo muszę poczekać, aż getCompanies w całości się wykona
-  const users = await getUsers();
-
-  for (let i = 0; i < companies.length; i++) {
-    const companyName = companies[i].name;
-    const newCompanyDiv = document.createElement('div');
-    newCompanyDiv.classList.add('companies');
-    newCompanyDiv.innerText = companyName;
-    table.appendChild(newCompanyDiv);
-
-    const newUserDiv = document.createElement('div');
-    newUserDiv.classList.add('users');
-
-    const filterFunction = user => {
-      return user.uris.company === companies[i].uri;
+    const companies = await getCompanies();
+    const users = await getUsers();
+    for (let i = 0; i < companies.length; i++) {
+        const companyName = companies[i].name;
+        const newCompanyDiv = document.createElement('div');
+        newCompanyDiv.classList.add('companies');
+        newCompanyDiv.innerText = companyName;
+        table.appendChild(newCompanyDiv);
+        const newUserDiv = document.createElement('div');
+        newUserDiv.classList.add('users');
+        const filterFunction = (user) => {
+            return user.uris.company === companies[i].uri;
+        };
+        newUserDiv.innerText = users.filter(filterFunction).map(user => user.name).toString();
+        table.appendChild(newUserDiv);
+        console.log('done');
     }
-
-    newUserDiv.innerText = users.filter(filterFunction).map(user => user.name);
-    table.appendChild(newUserDiv);
-    console.log('done');
-  }
-}
-
+};
 showData();
---
+++ app.ts
@@ -0,0 +1,47 @@
+const table = document.getElementById('table')! as HTMLDivElement;
+
+interface CompanyType {
+  name: string;
+  uri: string;
+}
+
+interface UserType {
+  name: string;
+  uris: {company: string};
+}
+
+const getUsers = async (): Promise<UserType[]> => {
+  const responseUsers = await fetch('http://localhost:3000/users');
+  return await responseUsers.json();
+};
+
+const getCompanies = async (): Promise<CompanyType[]> => {
+  const responseCompanies = await fetch('http://localhost:3000/companies');
+  return await responseCompanies.json();
+};
+
+const showData = async () => {
+  const companies = await getCompanies();   //await, bo muszę poczekać, aż getCompanies w całości się wykona
+  const users = await getUsers();
+
+  for (let i = 0; i < companies.length; i++) {
+    const companyName: string = companies[i].name;
+    const newCompanyDiv = document.createElement('div');
+    newCompanyDiv.classList.add('companies');
+    newCompanyDiv.innerText = companyName;
+    table.appendChild(newCompanyDiv);
+
+    const newUserDiv = document.createElement('div');
+    newUserDiv.classList.add('users');
+
+    const filterFunction = (user: UserType) => {
+      return user.uris.company === companies[i].uri;
+    }
+
+    newUserDiv.innerText = users.filter(filterFunction).map(user => user.name).toString();
+    table.appendChild(newUserDiv);
+    console.log('done');
+  }
+}
+
+showData();
